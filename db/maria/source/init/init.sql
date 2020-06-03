135d04c067c46dfe81d283a1d8aa08604eee8249
nanaones <nara03050@gmail.com>
2020-06-04 01:54:00 +0900

    feat: 초기 데이터베이스를 구성한다

---
+++ db/maria/source/init/init.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE test;
+CREATE TABLE test.test_table(
+   sdc_batched_at                   VARCHAR(50) 
+  ,sdc_level_0_id                   FLOAT
+  ,sdc_received_at                  VARCHAR(50)
+  ,sdc_sequence                     FLOAT
+  ,sdc_source_key_sdc_primary_key   VARCHAR(50)
+  ,sdc_table_version                FLOAT
+  ,value_                             VARCHAR(50)
+);
